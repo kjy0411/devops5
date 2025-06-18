f5757f7b6d1c36575a29372dac39bbbbed7ca8be
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2025-06-18 10:48:15 +0900

    Dockerfile 생성

--- src/main/java/com/sist/web/controller/MainController.java
+++ src/main/java/com/sist/web/controller/MainController.java
@@ -1,6 +1,12 @@
 package com.sist.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+// 서버 위치
+/*  
+ * 	maver => Framework => war
+ * 	gradle => Boot => jar (실행 가능한 파일)
+ *  
+ */
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
